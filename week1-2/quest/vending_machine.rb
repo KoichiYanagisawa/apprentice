c2d52f4c5f31c9dc1d31ee1184ebefb5f17e8e00
KoichiYanagisawa <tng62522@gmail.com>
2023-04-12 23:26:26 +0900

    全ての内容をアップデート

--- week1-2/quest/vending_machine.rb
+++ week1-2/quest/vending_machine.rb
@@ -8,28 +8,52 @@ class VendingMachine
     @manufacturer_name
   end
 
-  def press_button
-    if @coin >= 100
-      @coin -= 100
-      'cider'
+  def press_button(soft_drink)
+    case soft_drink.name
+    when 'cider'
+      if @coin >= 100
+        @coin -= 100
+        'cider'
+      end
+    when 'cola'
+      if @coin >= 150
+        @coin -= 150
+        'cola'
+      end
     end
   end
 
   def deposit_coin(coin)
     if(coin % 100 == 0)
-      @coin = coin
+      @coin += coin
     end
   end
   private :press_manufacturer_name
 end
 
+class SoftDrink
+  attr_reader :name, :price
+
+  def initialize(name)
+    @name = name
+    @price = drink_type
+  end
+
+  def drink_type
+    case @name
+    when 'cider'
+      100
+    when 'cola'
+      150
+    end
+  end
+end
+
+cola = SoftDrink.new('cola')
 vending_machine = VendingMachine.new('サントリー')
-puts vending_machine.press_button
-
-vending_machine.deposit_coin(150)
-puts vending_machine.press_button
-
 vending_machine.deposit_coin(100)
-puts vending_machine.press_button
-
-puts vending_machine.press_manufacturer_name
+print "出力1回目:"
+puts vending_machine.press_button(cola)
+vending_machine.deposit_coin(100)
+print "出力2回目:"
+puts vending_machine.press_button(cola)
