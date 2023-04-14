7186fe4ea84aec30ec72024c859546bbae469989
KoichiYanagisawa <tng62522@gmail.com>
2023-04-14 15:23:41 +0900

    gemfileでバージョン固定の練習

--- week1-2/quest/use_gem_faker.rb
+++ week1-2/quest/use_gem_faker.rb
@@ -1,11 +1,13 @@
 require 'faker/japanese'
+require 'faker'
 
 def faker(n)
-  (1..n).map {
+  (1..n).map do
     first_name = Faker::Japanese::Name.first_name
     last_name = Faker::Japanese::Name.last_name
-    "姓：#{last_name}\t名：#{first_name}"
-  }
+    "姓:#{last_name}\t名:#{first_name}"
+  end
 end
 
 puts faker(10)
+puts Faker::VERSION
