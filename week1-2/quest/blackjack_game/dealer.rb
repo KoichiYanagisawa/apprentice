aa841fee57ae15189c59b7d568f2dae96f55632c
KoichiYanagisawa <tng62522@gmail.com>
2023-04-23 22:42:52 +0900

    スーパークラスと同じインスタンス変数をそのまま使うので初期化のメソッド削除

--- week1-2/quest/blackjack_game/dealer.rb
+++ week1-2/quest/blackjack_game/dealer.rb
@@ -3,12 +3,7 @@
 require_relative 'player'
 # Playerクラスを継承したDealerクラス
 class Dealer < Player
-  def initialize(args = {})
-    super(args)
-    @way_of_calling = args[:way_of_calling] || default_way_of_calling
-  end
-
-  # デフォルトの呼ばれ方をここで指定
+  # デフォルトの呼ばれ方をここで指定(playerの同メソッドをオーバーライド)
   def default_way_of_calling
     'ディーラー'
   end
