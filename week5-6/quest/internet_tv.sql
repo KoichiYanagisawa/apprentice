c69511b92bedd7ad9e2dbd6e8ae0bee7da7cf84b
KoichiYanagisawa <tng62522@gmail.com>
2023-05-12 19:36:07 +0900

    サンプルデータを修正

--- week5-6/quest/internet_tv.sql
+++ week5-6/quest/internet_tv.sql
@@ -8,7 +8,7 @@ USE internet_tv;
 
 -- channels テーブルの作成
 CREATE TABLE channels (
-  channel_id INT(4) NOT NULL AUTO_INCREMENT,
+  channel_id INT(10) NOT NULL AUTO_INCREMENT,
   channel_name VARCHAR(100) NOT NULL,
   PRIMARY KEY (channel_id),
   UNIQUE (channel_name)
@@ -16,7 +16,7 @@ CREATE TABLE channels (
 
 -- series テーブルの作成
 CREATE TABLE series (
-  series_id INT(4) NOT NULL AUTO_INCREMENT,
+  series_id BIGINT(20) NOT NULL AUTO_INCREMENT,
   series_title VARCHAR(100) NOT NULL,
   PRIMARY KEY (series_id),
   UNIQUE (series_title)
@@ -24,18 +24,18 @@ CREATE TABLE series (
 
 -- seasons テーブルの作成
 CREATE TABLE seasons (
-  seasons_id INT(4) NOT NULL AUTO_INCREMENT,
-  series_id INT(4) NOT NULL,
+  season_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  series_id BIGINT(20) NOT NULL,
   season_num INT(2) NOT NULL,
   season_title VARCHAR(100) NOT NULL,
-  PRIMARY KEY (seasons_id),
+  PRIMARY KEY (season_id),
   FOREIGN KEY (series_id) REFERENCES series(series_id)
 );
 
 -- episodes テーブルの作成
 CREATE TABLE episodes (
-  episode_id INT(4) NOT NULL AUTO_INCREMENT,
-  seasons_id INT(4),
+  episode_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  season_id BIGINT(20),
   episode_title VARCHAR(100) NOT NULL,
   episode_num INT(4),
   playback_time TIME NOT NULL,
@@ -44,14 +44,14 @@ CREATE TABLE episodes (
   episode_views INT(10) NOT NULL,
   file_path VARCHAR(255) NOT NULL,
   PRIMARY KEY (episode_id),
-  FOREIGN KEY (seasons_id) REFERENCES seasons(seasons_id)
+  FOREIGN KEY (season_id) REFERENCES seasons(seasons_id)
 );
 
 -- program_list テーブルの作成
 CREATE TABLE program_list (
-  program_id INT(10) NOT NULL AUTO_INCREMENT,
-  channel_id INT(4) NOT NULL,
-  episode_id INT(4) NOT NULL,
+  program_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  channel_id INT(10) NOT NULL,
+  episode_id BIGINT(20) NOT NULL,
   program_list_title VARCHAR(100) NOT NULL,
   playback_datetime DATETIME NOT NULL,
   program_list_info VARCHAR(255),
@@ -72,7 +72,7 @@ CREATE TABLE genres (
 
 -- program_genres テーブルの作成
 CREATE TABLE program_genres (
-  program_genres_id INT(10) NOT NULL AUTO_INCREMENT,
+  program_genres_id BIGINT(20) NOT NULL AUTO_INCREMENT,
   genre_id INT(4) NOT NULL,
   program_id INT(10) NOT NULL,
   PRIMARY KEY (program_genres_id),
@@ -82,65 +82,46 @@ CREATE TABLE program_genres (
 
 -- 4.各テーブルに対し、サンプルデータを投入します。
 
--- channels
-INSERT INTO channels (channel_name) VALUES ('ドラマ');
-INSERT INTO channels (channel_name) VALUES ('アニメ');
-INSERT INTO channels (channel_name) VALUES ('バラエティ');
-INSERT INTO channels (channel_name) VALUES ('ドキュメンタリー');
+-- channelsテーブルにデータを挿入
+INSERT INTO channels (channel_name)
+VALUES ('Channel 1'), ('Channel 2'), ('Channel 3');
 
--- series
-INSERT INTO series (series_title) VALUES ('ドラマA');
-INSERT INTO series (series_title) VALUES ('ドラマB');
-INSERT INTO series (series_title) VALUES ('アニメA');
-INSERT INTO series (series_title) VALUES ('アニメB');
-INSERT INTO series (series_title) VALUES ('ドラマC');
-INSERT INTO series (series_title) VALUES ('アニメC');
-INSERT INTO series (series_title) VALUES ('バラエティA');
-INSERT INTO series (series_title) VALUES ('ドキュメンタリーA');
+-- seriesテーブルにデータを挿入
+INSERT INTO series (series_title)
+VALUES ('Series A'), ('Series B'), ('Series C'), ('Series D'), ('Series E'), ('Series F'), ('Series G'), ('Series H');
 
--- seasons
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (1, 1, 'ドラマA シーズン1');
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (1, 2, 'ドラマA シーズン2');
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (2, 1, 'ドラマB シーズン1');
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (3, 1, 'アニメA シーズン1');
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (4, 1, 'アニメB シーズン1');
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (5, 1, 'ドラマC シーズン1');
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (6, 1, 'アニメC シーズン1');
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (7, 1, 'バラエティA シーズン1');
-INSERT INTO seasons (series_id, season_num, season_title) VALUES (8, 1, 'ドキュメンタリーA シーズン1');
+-- genresテーブルにデータを挿入
+INSERT INTO genres (genre_name)
+VALUES ('Drama'), ('Comedy'), ('Action');
 
--- episodes
-INSERT INTO episodes (seasons_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path) VALUES (1, 'ドラマA シーズン1 エピソード1', 1, '00:45:00', '2023-05-11', 'ドラマA シーズン1 エピソード1の詳細', 5000, 'filepath1');
-INSERT INTO episodes (seasons_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path) VALUES (1, 'ドラマA シーズン1 エピソード2', 2, '00:45:00', '2023-05-12', 'ドラマA シーズン1 エピソード2の詳細', 4800, 'filepath2');
-INSERT INTO episodes (seasons_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path) VALUES (2, 'ドラマA シーズン2 エピソード1', 1, '00:45:00', '2023-05-13', 'ドラマA シーズン2 エピソード1の詳細', 5500, 'filepath3');
-INSERT INTO episodes (seasons_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path) VALUES (3, 'ドラマB シーズン1 エピソード1', 1, '00:45:00', '2023-05-14', 'ドラマB シーズン1 エピソード1の詳細', 5300, 'filepath4');
-INSERT INTO episodes (seasons_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path) VALUES (5, 'ドラマC シーズン1 エピソード1', 1, '00:45:00', '2023-05-16', 'ドラマC シーズン1 エピソード1の詳細', 4000, 'filepath6');
-INSERT INTO episodes (seasons_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path) VALUES (6, 'アニメC シーズン1 エピソード1', 1, '00:30:00', '2023-05-17', 'アニメC シーズン1 エピソード1の詳細', 4500, 'filepath7');
-INSERT INTO episodes (seasons_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path) VALUES (7, 'バラエティA シーズン1 エピソード1', 1, '01:00:00', '2023-05-18', 'バラエティA シーズン1 エピソード1の詳細', 4700, 'filepath8');
-INSERT INTO episodes (seasons_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path) VALUES (8, 'ドキュメンタリーA シーズン1 エピソード1', 1, '01:30:00', '2023-05-19', 'ドキュメンタリーA シーズン1 エピソード1の詳細', 4200, 'filepath9');
+-- seasonsテーブルにデータを挿入
+INSERT INTO seasons (series_id, season_num, season_title)
+VALUES (1, 1, 'Series A - Season 1'),
+       (1, 2, 'Series A - Season 2'),
+       (2, 1, 'Series B - Season 1'),
+       (2, 2, 'Series B - Season 2'),
+       (3, 1, 'Series C - Season 1'),
+       (4, 1, 'Series D - Season 1'),
+       (5, 1, 'Series E - Season 1'),
+       (6, 1, 'Series F - Season 1');
 
--- genres
-INSERT INTO genres (genre_name) VALUES ('ドラマ');
-INSERT INTO genres (genre_name) VALUES ('アニメ');
-INSERT INTO genres (genre_name) VALUES ('バラエティ');
-INSERT INTO genres (genre_name) VALUES ('ドキュメンタリー');
+-- episodesテーブルにデータを挿入
+INSERT INTO episodes (season_id, episode_title, episode_num, playback_time, release_date, episode_info, episode_views, file_path)
+VALUES (1, 'Episode 1', 1, '00:42:00', '2023-01-01', 'Series A - Season 1 - Episode 1', 100, 'file1.mp4'),
+       (1, 'Episode 2', 2, '00:45:00', '2023-01-08', 'Series A - Season 1 - Episode 2', 80, 'file2.mp4'),
+       (2, 'Episode 1', 1, '00:50:00', '2023-02-01', 'Series B - Season 1 - Episode 1', 120, 'file3.mp4'),
+       (2, 'Episode 2', 2, '00:48:00', '2023-02-08', 'Series B - Season 1 - Episode 2', 95, 'file4.mp4'),
+       (3, 'Episode 1', 1, '00:55:00', '2023-03-01', 'Series C - Season 1 - Episode 1', 130, 'file5.mp4'),
+       (4, 'Episode 1', 1, '00:53:00', '2023-04-01', 'Series D - Season 1 - Episode 1', 75, 'file6.mp4');
 
--- program_list
-INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views) VALUES (1, 1, 'ドラマA シーズン1 エピソード1', '2023-05-11 20:00:00', 'ドラマA シーズン1 エピソード1の詳細', 5000);
-INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views) VALUES (1, 2, 'ドラマA シーズン1 エピソード2', '2023-05-12 20:00:00', 'ドラマA シーズン1 エピソード2の詳細', 4800);
-INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views) VALUES (1, 3, 'ドラマA シーズン2 エピソード1', '2023-05-13 20:00:00', 'ドラマA シーズン2 エピソード1の詳細', 5500);
-INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views) VALUES (1, 4, 'ドラマB シーズン1 エピソード1', '2023-05-14 20:00:00', 'ドラマB シーズン1 エピソード1の詳細', 5300);
-INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views) VALUES (1, 5, 'ドラマC シーズン1 エピソード1', '2023-05-16 20:00:00', 'ドラマC シーズン1 エピソード1の詳細', 4000);
-INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views) VALUES (2, 6, 'アニメC シーズン1 エピソード1', '2023-05-17 19:00:00', 'アニメC シーズン1 エピソード1の詳細', 4500);
-INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views) VALUES (3, 7, 'バラエティA シーズン1 エピソード1', '2023-05-18 21:00:00', 'バラエティA シーズン1 エピソード1の詳細', 4700);
-INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views) VALUES (4, 8, 'ドキュメンタリーA シーズン1 エピソード1', '2023-05-19 22:00:00', 'ドキュメンタリーA シーズン1 エピソード1の詳細', 4200);
+-- program_listテーブルにデータを挿入
+INSERT INTO program_list (channel_id, episode_id, program_list_title, playback_datetime, program_list_info, program_list_views)
+VALUES (1, 1, 'Channel 1 - Episode 1', '2023-05-01 20:00:00', 'Series A - Season 1 - Episode 1 on Channel 1', 50),
+       (1, 2, 'Channel 1 - Episode 2', '2023-05-08 20:00:00', 'Series A - Season 1 - Episode 2 on Channel 1', 40),
+       (2, 3, 'Channel 2 - Episode 1', '2023-05-02 21:00:00', 'Series B - Season 1 - Episode 1 on Channel 2', 60),
+       (2, 4, 'Channel 2 - Episode 2', '2023-05-09 21:00:00', 'Series B - Season 1 - Episode 2 on Channel 2', 55),
+       (3, 5, 'Channel 3 - Episode 1', '2023-05-03 22:00:00', 'Series C - Season 1 - Episode 1 on Channel 3', 70);
 
--- program_genres
-INSERT INTO program_genres (genre_id, program_id) VALUES (1, 1);
-INSERT INTO program_genres (genre_id, program_id) VALUES (1, 2);
-INSERT INTO program_genres (genre_id, program_id) VALUES (1, 3);
-INSERT INTO program_genres (genre_id, program_id) VALUES (1, 4);
-INSERT INTO program_genres (genre_id, program_id) VALUES (1, 5);
-INSERT INTO program_genres (genre_id, program_id) VALUES (2, 6);
-INSERT INTO program_genres (genre_id, program_id) VALUES (3, 7);
-INSERT INTO program_genres (genre_id, program_id) VALUES (4, 8);
+-- program_genresテーブルにデータを挿入
+INSERT INTO program_genres (genre_id, program_id)
+VALUES (1, 1), (1, 2), (2, 3), (2, 4), (3, 5);
